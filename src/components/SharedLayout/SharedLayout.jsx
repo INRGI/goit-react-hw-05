594a0eb2878dc451a08c41f9811a54704ef2cec0
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-01-26 11:01:37 +0100

    Header

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,16 +1,22 @@
 import { Suspense } from "react";
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { Header, Logo, LogoContainer, StyledLink } from "./SharedLayout.styled";
+import { FcFilm } from "react-icons/fc";
 
 const SharedLayout = () => {
     return (
         <div>
-            <header>
-                <div className="logo"></div>
+            <Header>
+                <LogoContainer>
+                    <FcFilm size={33} color="black" />
+                    <Logo>Movies</Logo>
+                </LogoContainer>
+                
                 <nav>
-                    <NavLink to="/" end>Home</NavLink>
-                    <NavLink to="/movies">Movies</NavLink>
+                    <StyledLink to="/" end>Home</StyledLink>
+                    <StyledLink to="/movies">Movies</StyledLink>
                 </nav>
-            </header>
+            </Header>
             <Suspense fallback={<div>Loading page...</div>}>
                 <Outlet />
             </Suspense>
---
+++ src/components/SharedLayout/SharedLayout.styled.jsx
@@ -0,0 +1,44 @@
+import styled from '@emotion/styled';
+import { NavLink } from "react-router-dom";
+
+export const Header = styled.header`
+    background-color: #333;
+    display: flex;
+    height: 52px;
+    align-items: center;
+    box-shadow: rgba(0, 0, 0, 0.45) 0px 25px 20px -20px;
+`;
+
+export const StyledLink = styled(NavLink)`
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 18px;
+    font-weight: bold;
+    transition: background-color ease-in-out 0.1s;
+    &:hover {
+        background-color: #111;
+    }
+
+  &.active {
+    background-color: #BD00FF;
+  }
+`;
+
+export const LogoContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-left:30px;
+`;
+
+export const Logo = styled.p`
+    margin-left: 5px;
+    margin-right:30px;
+    margin: 0 30px 0 5px;
+    color: white;
+    font-size: 28px;
+    font-weight: bold;
+    letter-spacing: 0.02em;
+`;
