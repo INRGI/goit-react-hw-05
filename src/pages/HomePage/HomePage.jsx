8f2bfb1d127b576472779358015fc2c79047e4cf
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-01-28 12:49:43 +0100

    Delete sceleton

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import { fetchTrendingMoviesPages } from '../../services/TMDBAPI';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import MoviesList from '../../components/MoviesList/MoviesList';
+import Loader from '../../components/Loader'
 import Pagination from '@mui/material/Pagination';
 import { Container, Title } from './HomePage.styled';
 import { VscFlame } from "react-icons/vsc";
@@ -12,6 +12,7 @@ const HomePage = () => {
     const [trendingMovies, setTrendingMovies] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleChange = (event, value) => {
         setCurrentPage(value);
@@ -21,13 +22,17 @@ const HomePage = () => {
     useEffect(() => {
         const fetchMovies = async () => {
             try {
+                setIsLoading(true);
                 const data = await fetchTrendingMoviesPages(currentPage);
                 setTrendingMovies(data.results);
                 setTotalPages(data.total_pages);
+                setIsLoading(false);
             } catch {
                 toast.error('Something went wrong 😿', {
                     position: 'top-right',
                 });
+            } finally {
+                setIsLoading(false);
             }
         };
         fetchMovies();
@@ -36,9 +41,9 @@ const HomePage = () => {
     return (
         <Container>
             <Title>Trending Today <VscFlame size={30}/></Title>
-            <SkeletonTheme baseColor="#202020" highlightColor="#444">
-                {trendingMovies.length === 0 ? (
-                    <Skeleton count={20} />
+
+                {isLoading ? (
+                    <Loader />
                 ) : (
                     <>
                         <MoviesList movies={trendingMovies} />
@@ -51,7 +56,7 @@ const HomePage = () => {
                         />
                     </>
                 )}
-            </SkeletonTheme>
+
             <ToastContainer />
         </Container>
     );
--- src/pages/HomePage/HomePage.styled.jsx
+++ src/pages/HomePage/HomePage.styled.jsx
@@ -19,3 +19,23 @@ export const Title = styled.h2`
    white-space:nowrap;
    color: #910505;
 `;
+
+
+export const Ul = styled.ul`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  max-width: calc(100vw - 48px);
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  grid-gap: 16px;
+  margin-top: 0;
+  margin-bottom: 20px;
+  padding: 0;
+  list-style: none;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const Li = styled.li`
+    border-radius: 10px;
+`;
